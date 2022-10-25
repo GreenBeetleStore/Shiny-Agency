38e5deb5ddc4ad0c4de2af0cb6ccbd60208e86e5
XT Escarabat Verd <f.xaviertorta@gmail.com>
2022-10-25 23:56:56 +0200

    useState et useEffect pour effectuer des calls API

--- src/pages/Freelances/index.jsx
+++ src/pages/Freelances/index.jsx
@@ -1,6 +1,8 @@
+import { useEffect, useState } from 'react'
 import Card from '../../components/Card'
 import styled from 'styled-components'
 import colors from '../../utils/style/colors'
+import { Loader } from '../../utils/style/Atoms'
 
 const CardsContainer = styled.div`
    display: grid;
@@ -25,38 +27,59 @@ const PageSubtitle = styled.h2`
    text-align: center;
    padding-bottom: 30px;
 `
-
-const freelanceProfiles = [
-   {
-      name: 'Jane Doe',
-      jobTitle: 'Devops',
-   },
-   {
-      name: 'John Doe',
-      jobTitle: 'Developpeur frontend',
-   },
-   {
-      name: 'Jeanne Biche',
-      jobTitle: 'Développeuse Fullstack',
-   },
-]
+const LoaderWrapper = styled.div`
+   display: flex;
+   justify-content: center;
+`
 
 function Freelances() {
+   const [isDataLoading, setDataLoading] = useState(false)
+   const [error, setError] = useState(false)
+   const [freelancersList, setFreelancesList] = useState([])
+
+   useEffect(() => {
+      async function fetchFreelances() {
+         setDataLoading(true)
+         try {
+            const response = await fetch(`http://localhost:8000/freelances`)
+            const { freelancersList } = await response.json()
+            setFreelancesList(freelancersList)
+         } catch (err) {
+            console.log('===== error =====', err)
+            setError(true)
+         } finally {
+            setDataLoading(false)
+         }
+      }
+      fetchFreelances()
+   }, [])
+
+   if (error) {
+      return <span>Oups il y a eu un problème</span>
+   }
+
    return (
       <div>
          <PageTitle>Trouvez votre prestataire</PageTitle>
          <PageSubtitle>
             Chez Shiny nous réunissons les meilleurs profils pour vous.
          </PageSubtitle>
-         <CardsContainer>
-            {freelanceProfiles.map((profile, index) => (
-               <Card
-                  key={`${profile.name}-${index}`}
-                  label={profile.jobTitle}
-                  title={profile.name}
-               />
-            ))}
-         </CardsContainer>
+         {isDataLoading ? (
+            <LoaderWrapper>
+               <Loader />
+            </LoaderWrapper>
+         ) : (
+            <CardsContainer>
+               {freelancersList.map((profile, index) => (
+                  <Card
+                     key={`${profile.name}-${index}`}
+                     label={profile.job}
+                     title={profile.name}
+                     picture={profile.picture}
+                  />
+               ))}
+            </CardsContainer>
+         )}
       </div>
    )
 }
--- src/pages/Survey/index.jsx
+++ src/pages/Survey/index.jsx
@@ -1,23 +1,84 @@
+import { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+import colors from '../../utils/style/colors'
+import { Loader } from '../../utils/style/Atoms'
+
+const SurveyContainer = styled.div`
+   display: flex;
+   flex-direction: column;
+   align-items: center;
+`
+
+const QuestionTitle = styled.h2`
+   text-decoration: underline;
+   text-decoration-color: ${colors.primary};
+`
+
+const QuestionContent = styled.span`
+   margin: 30px;
+`
+
+const LinkWrapper = styled.div`
+   padding-top: 30px;
+   & a {
+      color: black;
+   }
+   & a:first-of-type {
+      margin-right: 20px;
+   }
+`
 
 function Survey() {
-  const { questionNumber } = useParams()
-  const questionNumberInt = parseInt(questionNumber)
-  const prevQuestionNumber = questionNumberInt === 1 ? 1 : questionNumberInt - 1
-  const nextQuestionNumber = questionNumberInt + 1
-  return (
-    <div>
-      <h1>Questionnaire 🧮</h1>
-      <h2>Question {questionNumber}</h2>
-      <Link to={`/survey/${prevQuestionNumber}`}>Précédent</Link>
-      {questionNumberInt === 10 ? (
-        <Link to="/results">Résultats</Link>
-      ) : (
-        <Link to={`/survey/${nextQuestionNumber}`}>Suivant</Link>
-      )}
-    </div>
-  )
+   const { questionNumber } = useParams()
+   const questionNumberInt = parseInt(questionNumber)
+   const prevQuestionNumber =
+      questionNumberInt === 1 ? 1 : questionNumberInt - 1
+   const nextQuestionNumber = questionNumberInt + 1
+   const [surveyData, setSurveyData] = useState({})
+   const [isDataLoading, setDataLoading] = useState(false)
+   const [error, setError] = useState(false)
+
+   useEffect(() => {
+      async function fetchSurvey() {
+         setDataLoading(true)
+         try {
+            const response = await fetch(`http://localhost:8000/survey`)
+            const { surveyData } = await response.json()
+            setSurveyData(surveyData)
+         } catch (err) {
+            console.log(err)
+            setError(true)
+         } finally {
+            setDataLoading(false)
+         }
+      }
+      fetchSurvey()
+   }, [])
+
+   if (error) {
+      return <span>Oups il y a eu un problème</span>
+   }
+
+   return (
+      <SurveyContainer>
+         <QuestionTitle>Question {questionNumber}</QuestionTitle>
+         {isDataLoading ? (
+            <Loader />
+         ) : (
+            <QuestionContent>{surveyData[questionNumber]}</QuestionContent>
+         )}
+         <LinkWrapper>
+            <Link to={`/survey/${prevQuestionNumber}`}>Précédent</Link>
+            {surveyData[questionNumberInt + 1] ? (
+               <Link to={`/survey/${nextQuestionNumber}`}>Suivant</Link>
+            ) : (
+               <Link to="/results">Résultats</Link>
+            )}
+         </LinkWrapper>
+      </SurveyContainer>
+   )
 }
 
 export default Survey
